ba2d132de0cf732854fa70561916689fb6336ac7
UBONGO2000 <gadyubongo@gmail.com>
2026-03-20 10:41:08 +0100

    correction cors 2

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: ['https://prompt-hub-frontend-kmni81y11-ubongo2000s-projects.vercel.app/'],
+    origin: ['https://prompt-hub-frontend-kmni81y11-ubongo2000s-projects.vercel.app/','https://prompt-hub-frontend-dun.vercel.app/'],
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
